2dcaef51a4476748ee3a6cb3d6f191868fff6e40
Carlos Beuret <guillermobeuret@hotmail.com>
2023-01-25 19:10:37 -0300

    creacion del contenedor streamlit para ml

--- machine_learning/app/app.py
+++ machine_learning/app/app.py
@@ -1,12 +1,16 @@
-import streamlit as st, pandas as pd, numpy as np, sqlalchemy as sql
-import statsmodels.api as sm, matplotlib.pyplot as plt, pickle as pkl
+import streamlit as st
+import pandas as pd
+import sqlalchemy as sql
+import pickle as pkl
+
 
 st.title('Forecasting Olist Sales')
 st.header('1.-  ARIMA')
 
 engine = sql.create_engine(
-    "mysql+pymysql://root:password@localhost:3307/data_warehouse_olist?charset=utf8mb4"
+    "mysql+pymysql://root:password@mysql:3306/data_warehouse_olist?charset=utf8mb4"
 )
+
 DF = pd.read_sql( 'select o.purchase_timestamp, oi.price from orders as o inner join order_items as oi on o.order_id = oi.order_id', con=engine)
 DF['purchase_timestamp']=pd.to_datetime(DF['purchase_timestamp'])
 
@@ -22,7 +26,7 @@ future_dates=[Sales_per_Week.index[-1]+ DateOffset(weeks=x)for x in range(0,32)]
 future_dates_df=pd.DataFrame(index=future_dates[1:],columns=Sales_per_Week.columns)
 future_df=pd.concat([Sales_per_Week,future_dates_df])
 
-with open ('total_model.pkl','rb') as f:
+with open ('app/total_model.pkl','rb') as f:
     total_model = pkl.load(f)
 
 date = st.select_slider('select the date', options=future_dates)
@@ -33,6 +37,20 @@ st.write('the sales prediction is', future_df['forecast'].dropna())
 
 st.header('2.-  Prophet')
 
+st.header('3.-Modelo de recomendación-producto')
 
+product = st.text_input("Field 1")
+
+btn = st.button("predict")
+def recommend_products(product_id):
+        df_ml= pd.read_pickle('app/recomendacion_producto.pkl')
+        filtro_aux = df_ml['product_id'] == product
+        categoria = df_ml[filtro_aux]['category_name']
+        group = df_ml[filtro_aux]['group']
+        filtro = (df_ml['group'] == group.values[0]) & (df_ml['category_name'] == categoria.values[0]) & (df_ml['product_id']!= product)
+        df =df_ml[filtro].sort_values(by='ventas_producto', ascending=False)
+        return df.head(3)
+if btn:
+    st.write(recommend_products(product).head(3))
 
 
