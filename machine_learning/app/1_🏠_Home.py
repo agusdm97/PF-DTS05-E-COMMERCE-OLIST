e7ad95750d826b7246be52efe8f81748a8b7180d
alkymvaletta <alkymvaletta@gmail.com>
2023-01-30 18:51:56 -0500

    Ajuste de home para ML

--- machine_learning/app/1_🏠_Home.py
+++ machine_learning/app/1_🏠_Home.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from PIL import Image
 
 
-st.write("# Olist's maching learning models")
+col1, col2, col3 = st.columns([1, 6, 1])
+
+with col2:
+    st.header("Olist's maching learning models")
+    image = Image.open("src/brain.png")
+    st.write("")
+    st.image(image, caption="")
